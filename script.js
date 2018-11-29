ac71bacc8d3d2396f99a28c78e8a2e802c8cc44b
samuel413 <44417462+samuel413@users.noreply.github.com>
2018-11-29 15:55:35 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#button1").click(function() {
+    $("#first").slideToggle();
+});
+
+$("#button2").click(function() {
+    $("#second").toggle();
+});
+
+$("#button3").click(function() {
+    $("#third").fadeToggle();
+});
